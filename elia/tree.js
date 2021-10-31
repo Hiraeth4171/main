0143ff36fdfbbfd0d43b3fc55cc9c86705c38ba9
Hiraeth <hiraeth4171@gmail.com>
2021-10-31 20:21:49 +0200

    boop

--- elia/tree.js
+++ elia/tree.js
@@ -1,4 +1,5 @@
 let nodes = [];
+let selectedNodeID = -1;
 
 class Node {
     constructor(name, parentID, method) {
@@ -13,13 +14,35 @@ class Node {
         this.name = name;
     }
 
-    html() {
-        return `<div id="${this.id}" class="node">${this.name}</div>`
+    html(br) {
+        return `<div id="${br ? 'n'+this.id : this.id}" class="node">${this.name}</div>`
     }
 }
 
-let first = new Node("fuck", 0, "by fucking");
-document.getElementById('tree').appendChild(document.createRange().createContextualFragment(first.html()));
+let first = new Node("boom", -1, "by fucking");
+for(let i = 0; i < 10; i++) {
+    new Node(Math.random(), i, i.toString());
+    new Node(Math.random(), i, i.toString());
+    new Node(Math.random(), i, i.toString());
+}
+
+let render = () => {
+    for(let i = 0;i < nodes.length; i++){
+        let node = nodes[i];
+        let dest = 'tree';
+        if(node.parentID != -1){
+            dest = ('row'+node.parentID).toString();
+            console.log(dest);
+        }
+        if(document.getElementById(dest) == null) {
+            let d = document.createElement("div");
+            d.setAttribute("id", dest)
+            d.setAttribute("class", "node-row")
+            document.getElementById('tree').appendChild(d);
+        }
+        document.getElementById(dest).appendChild(document.createRange().createContextualFragment(node.html(dest == 'tree' ? true : false)));
+    }
+}
 
 
 /**
@@ -28,15 +51,22 @@ document.getElementById('tree').appendChild(document.createRange().createContext
  * 
  */
 
+let getNodeObjById = (id) => {
+    return document.getElementById(id);
+}
 
 document.addEventListener("click",(e)=> {
-    e.preventDefault();
     if(e.target.classList.contains("node")) {
         e.target.innerHTML = `<input type="text" id="${e.target.getAttribute("id")}" class="node-textbox" value="${nodes[e.target.getAttribute("id")].name}"></input>`
+        selectedNodeID = e.target.getAttribute("id");
     }
     else {
-        if(e.target.outerHTML != e.target.value) e.target.outerHTML = e.target.value
-        if(e.target.value != nodes[e.target.getAttribute("id")]) nodes[e.target.getAttribute("id")].rename(e.target.value);
+        if(e.target.classList.contains("node-textbox") || selectedNodeID == -1) return;
+        let node = getNodeObjById(selectedNodeID.toString());
+        console.log(node);
+        nodes[selectedNodeID].rename(node.firstChild.value);
+        node.innerHTML =  node.firstChild.value;
+        selectedNodeID = -1;
     }
 })
 
@@ -48,3 +78,5 @@ document.addEventListener("keydown", (e)=> {
         nodes[e.target.getAttribute("id")].rename(e.target.value);
     }
 })
+
+render();
