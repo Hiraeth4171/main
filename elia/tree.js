793ea1bb2acae5f931682882da2c4605c506570a
Hiraeth4171 <hiraeth4171@gmail.com>
2021-11-03 16:32:59 +0000

    added elia as a project moment

--- elia/tree.js
+++ elia/tree.js
@@ -15,26 +15,26 @@ class Node {
     }
 
     html(br) {
-        return `<div id="${br ? 'n'+this.id : this.id}" class="node">${this.name}</div>`
+        return `<div id="${br ? 'n' + this.id : this.id}" class="node">${this.name}</div>`
     }
 }
 
 let first = new Node("boom", -1, "by fucking");
-for(let i = 0; i < 10; i++) {
+for (let i = 0; i < 10; i++) {
     new Node(Math.random(), i, i.toString());
     new Node(Math.random(), i, i.toString());
     new Node(Math.random(), i, i.toString());
 }
 
 let render = () => {
-    for(let i = 0;i < nodes.length; i++){
+    for (let i = 0; i < nodes.length; i++) {
         let node = nodes[i];
         let dest = 'tree';
-        if(node.parentID != -1){
-            dest = ('row'+node.parentID).toString();
+        if (node.parentID != -1) {
+            dest = ('row' + node.parentID).toString();
             console.log(dest);
         }
-        if(document.getElementById(dest) == null) {
+        if (document.getElementById(dest) == null) {
             let d = document.createElement("div");
             d.setAttribute("id", dest)
             d.setAttribute("class", "node-row")
@@ -55,23 +55,23 @@ let getNodeObjById = (id) => {
     return document.getElementById(id);
 }
 
-document.addEventListener("click",(e)=> {
-    if(e.target.classList.contains("node")) {
+document.addEventListener("click", (e) => {
+    if (e.target.classList.contains("node")) {
         e.target.innerHTML = `<input type="text" id="${e.target.getAttribute("id")}" class="node-textbox" value="${nodes[e.target.getAttribute("id")].name}"></input>`
         selectedNodeID = e.target.getAttribute("id");
     }
     else {
-        if(e.target.classList.contains("node-textbox") || selectedNodeID == -1) return;
+        if (e.target.classList.contains("node-textbox") || selectedNodeID == -1) return;
         let node = getNodeObjById(selectedNodeID.toString());
         console.log(node);
         nodes[selectedNodeID].rename(node.firstChild.value);
-        node.innerHTML =  node.firstChild.value;
+        node.innerHTML = node.firstChild.value;
         selectedNodeID = -1;
     }
 })
 
-document.addEventListener("keydown", (e)=> {
-    if(e.key == 'Enter' && e.target.classList.contains("node-textbox")) {
+document.addEventListener("keydown", (e) => {
+    if (e.key == 'Enter' && e.target.classList.contains("node-textbox")) {
         console.log(e.target);
         e.target.outerHTML = e.target.value;
         console.log(e.target.getAttribute("id"));
