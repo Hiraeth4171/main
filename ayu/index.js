987692fc0b1f28e993eeab0ab579f457493c21f6
Hiraeth <Hiraeth4171@gmail.com>
2022-02-15 23:14:41 +0200

    ayu

---
+++ ayu/index.js
@@ -0,0 +1,57 @@
+let toggleCanvas = 1;
+
+$(document).ready(async () => {
+
+    $('#ayu').mouseover(function (e) {
+        $('#gif').css('opacity', 1);
+    });
+
+    $('#ayu').mouseleave(function (e) {
+        $('#gif').css('opacity', 0);
+    });
+
+    $('#ayu').click(function(e) {
+        e.preventDefault();
+        $('#ayumoment > canvas').css('opacity', toggleCanvas);
+        toggleCanvas == 0 ? toggleCanvas = 1 : toggleCanvas = 0;
+    })
+
+    let randomWord = await request("https://random-word-api.herokuapp.com/word?swear=1", {})
+    let translatedWord = await request("https://libretranslate.de/translate", {
+        method: "POST",
+        body: JSON.stringify({
+            q: randomWord,
+            source: "en",
+            target: "ja",
+            format: "text"
+        }),
+        headers: {
+            "Content-Type": "application/json",
+        }
+    })
+    let definition = await request(`https://api.dictionaryapi.dev/api/v2/entries/en/${randomWord}`, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+        }
+    })
+    let jpw = translatedWord.translatedText[0]
+    $('#writing').append(`<p>Kana : ${wanakana.toKana(jpw)}</p><p>Katakana : ${wanakana.toKatakana(jpw)}</p><p>Hiragana : ${wanakana.toHiragana(jpw)}</p><p>Romaji : ${wanakana.toRomaji(jpw)}</p>`)
+    $('#word').html(jpw + " - " + randomWord);
+    $("#def").text(definition ? definition[0].meanings[0].partOfSpeech + " - " + definition[0].meanings[0].definitions[0].definition : "Sorry - Definition Unavailable")
+});
+
+async function request(url, options) {
+    let returnValue = 0;
+    await fetch(url, options).then(res => {
+        if (!res.ok) {
+            throw new Error("HTTP error " + res.status);
+        }
+        return res.json();
+    }).then(json => {
+        returnValue = json
+    }).catch(err => {
+        alert(err)
+    })
+    return returnValue;
+}
---
+++ ayu/sketch.js
@@ -0,0 +1,82 @@
+let w = window.innerWidth,
+    h = window.innerHeight,
+    particles = [],
+    speedMod = 2,
+    particleCount = 45;
+
+let toggle = true;
+class Particle {
+    constructor() { 
+        this.y = 0;
+        this.x = random(0, w);
+        this.r = Math.floor(random(3, 7));
+        this.speed = random(2, 10);
+    }
+
+    update() {
+        this.y += speedMod * this.speed;
+    }
+
+    show() {
+        fill(54, 84, 49);
+        noStroke();
+        drawGradient(this.x, this.y, this.r / 2, this.r / 2);
+        noFill()
+        stroke(54, 84, 49, (this.speed / 5) * 255);
+        strokeWeight(this.r / this.speed)
+        line(this.x, this.y, this.x, this.y - this.speed * 15);
+        stroke(54, 84, 49, (this.speed / 5) * 100);
+        strokeWeight((this.r / this.speed) * 4)
+        line(this.x, this.y, this.x, this.y - this.speed * 15);
+    }
+
+    splash() {
+        // fill(54, 84, 49);
+        // ellipse(this.x, h / 2 + 50, this.r * 15, this.r * 13);
+        drawGradient(this.x, h / 2 +40, this.r * 0.15, this.r * 0.13);
+        fill(54, 84, 49, 30);
+        ellipse(this.x, h / 2 +40, this.r * 5, this.r * 3)
+        strokeWeight(5)
+        line(this.x - 20, h / 2 +40, this.x + 20, h / 2 +40);
+    }
+}
+
+function setup() {
+    let canvas = createCanvas(w, h / 2 + 40);
+    canvas.parent("ayumoment");
+    for (let i = 0; i < particleCount; i++) {
+        particles[i] = new Particle();
+    }
+}
+
+function draw() {
+    if(!toggle) return;
+    clear(0);
+    stroke(54, 84, 49, 40);
+    strokeWeight(3);
+    line(0, h / 2 +40, w, h / 2 + 40)
+    if (particles.length < particleCount) while (particles.length - particleCount) particles.push(new Particle());
+    for (let i = 0; i < particles.length; i++) {
+        let particle = particles[i];
+        if (particle.y >= (h / 2 + (particle.r * 10))) {
+            particle.update();
+            particle.splash();
+            particle.show()
+            if (particle.y > (h / 2 + (particle.r * 10)) + (particle.speed * 15)) particles.splice(i, 1);
+        }
+        else {
+            particle.update()
+            particle.show()
+        }
+    }
+}
+
+function drawGradient(x, y, r1, r2) {
+    let h = 0;
+    for (let r = r2; r > 0; --r) {
+        r < (r2 - r1) * 50 ? fill(54, 84, 49, 255) : fill(54, 84, 49, h);
+        ellipse(x, y, r * 10, r * 10);
+
+        h += 20;
+    }
+}
